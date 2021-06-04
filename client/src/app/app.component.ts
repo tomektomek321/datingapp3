9fb8473bc2dc4d2cf4e4acfa0de11eb096da5a34
Tomek <tomek1989gw@wppl>
2021-06-04 20:29:40 +0200

    Client: test http request

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+    title = 'client';
+
+    constructor(private http: HttpClient) { }
+
+
+    ngOnInit(): void {
+        this.http.get("https://localhost:5001/api/users").subscribe( (response) => {
+            console.log(response)
+        }, error => {
+            console.log(error)
+        } )
+    }
+
+
+
 }
