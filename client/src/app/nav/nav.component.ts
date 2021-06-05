74f56f15a646689f969ef5da1fd9293749a82353
Tomek <tomek1989gw@wppl>
2021-06-05 22:00:23 +0200

    Client: test toarst

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -13,7 +14,10 @@ export class NavComponent implements OnInit {
     model: any = {}
     currentUser$: Observable<User>
 
-    constructor(private accountService: AccountService, private router: Router) { }
+    constructor(
+        private accountService: AccountService,
+        private router: Router,
+        private toastr: ToastrService) { }
 
     ngOnInit(): void {
         this.currentUser$ = this.accountService.currentUser$
@@ -25,6 +29,7 @@ export class NavComponent implements OnInit {
             this.router.navigateByUrl('/members')
         }, error => {
             console.log(error)
+            this.toastr.error(error.error);
         })
     }
 
