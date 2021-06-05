67cadf2b426ebd1219deacdc7fd5a4057414bf82
Tomek <tomek1989gw@wppl>
2021-06-05 03:18:13 +0200

    Client: Basic account service

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+    selector: 'app-nav',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+    model: any = []
+    loggedIn: boolean
 
-  constructor() { }
+    constructor(private accountService: AccountService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+
+    }
+
+    login() {
+        this.accountService.login(this.model).subscribe(response => {
+            console.log(response)
+            this.loggedIn = true
+        }, error => {
+            console.log(error)
+        })
+    }
 
 }
