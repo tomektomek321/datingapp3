c5ed8a0b731f7a90dfd483d9bfe50f80bb6a6ae7
Tomek <tomek1989gw@wppl>
2021-06-11 03:52:00 +0200

    Client: Lists component class

--- client/src/app/members/lists/lists.component.ts
+++ client/src/app/members/lists/lists.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { Pagination } from 'src/app/_models/pagination';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
-  selector: 'app-lists',
-  templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.scss']
+    selector: 'app-lists',
+    templateUrl: './lists.component.html',
+    styleUrls: ['./lists.component.scss']
 })
 export class ListsComponent implements OnInit {
+    members: Partial<Member[]>
+    predicate = 'liked'
+    pageNumber = 1
+    pageSize = 5
+    pagination: Pagination
 
-  constructor() { }
+    constructor(private memberService: MembersService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.loadLikes()
+    }
+
+    loadLikes() {
+        this.memberService.getLikes(this.predicate, this.pageNumber, this.pageSize).subscribe(response => {
+            this.members = response.result
+            this.pagination = response.pagination
+        })
+    }
+
+    pageChanged(event: any) {
+        this.pageNumber = event.page
+        this.loadLikes()
+    }
 
 }
