96cf489228262002d59c9617fc182e6119d8bb2e
Tomek <tomek1989gw@wppl>
2021-06-11 04:07:51 +0200

    Client: MembersService addLikes

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -110,4 +110,14 @@ export class MembersService {
         return params;
     }
 
+    addLike(username: string) {
+        return this.http.post(this.baseUrl + 'likes/' + username, {})
+    }
+
+    getLikes(predicate: string, pageNumber, pageSize) {
+        let params = this.getPaginationHeaders(pageNumber, pageSize)
+        params = params.append('predicate', predicate)
+        return this.getPaginatedResult<Partial<Member[]>>(this.baseUrl + 'likes', params)
+    }
+
 }
