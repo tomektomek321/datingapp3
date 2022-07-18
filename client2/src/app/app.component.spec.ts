900ca773f76acddb15d6398bad0d6854567ffa75
Tomek <tomek1989gw@wp.pl>
2022-07-18 19:07:12 +0200

    fixed appComponent tests

--- client2/src/app/app.component.spec.ts
+++ client2/src/app/app.component.spec.ts
@@ -1,8 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+
     beforeEach(async () => {
         await TestBed.configureTestingModule({
         imports: [
@@ -11,35 +15,52 @@ describe('AppComponent', () => {
         declarations: [
             AppComponent
         ],
+        providers: [
+
+        ]
         }).compileComponents();
+
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+
     });
 
+
     it('should create the app', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app).toBeTruthy();
+        expect(component).toBeTruthy();
     });
 
     it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
         const selector = fixture.nativeElement as HTMLElement;
-        expect(selector.querySelector('h1')?.textContent).toContain('App is working');
+        expect(selector.querySelector('h1')?.textContent).toContain('Hello');
     });
 
     it('should render title 2', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
         const selector = fixture.nativeElement as HTMLElement;
-        expect(selector.querySelector('h1')?.innerHTML).toBe('App is working');
+        expect(selector.querySelector('h1')?.innerHTML).toBe('Hello');
     });
 
-    it('should X variable to be "Hej"', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const instance = fixture.componentInstance;
-        fixture.detectChanges();
-        const value = instance.x;
-        expect(value).toBe('hej');
+    it('x variable to be "Hello"', () => {
+        const value = component.x;
+        expect(value).toEqual('Hello');
     });
+
+    it('should X variable change value to Hello2', (done) => {
+        component.x = "Hello2";
+        fixture.detectChanges();
+        fixture.whenStable().then(res => {
+            const newValue = fixture.debugElement.nativeElement.querySelector('h1').textContent;
+            expect(newValue).toEqual("Hello2");
+            done();
+        });
+    });
+
+
+
+
+    // element.getAttrivute("placeholder")
+    // element.dataset.widget
+    // element['data'widget'] .. // ???
 
 });
--- client2/src/app/app.component.ts
+++ client2/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    public x = "hej";
+    x = "Hello";
 }
